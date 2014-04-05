0e3346c53c2844fd060451953e6db1514963673c
Denis Krayushkin <krayud@yandex.ru>
2014-04-05 23:24:27 +0700

    First git mac os consile

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FirstApp::Application.config.secret_key_base = '07834b960fd055a1624e3be283e983647854315322ff0c436244ee43157312c382533cbd8b07d4893b1da3a578694e68b98ccdaacfc4150f6f19aec08837f58f'
